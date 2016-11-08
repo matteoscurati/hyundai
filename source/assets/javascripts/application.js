ceb8cc0815069a720a0cba5bdc29576a675168d0
Matteo Scurati <matteo.scurati@gmail.com>
2016-11-08 15:26:09 +0100

    Add article

--- source/assets/javascripts/application.js
+++ source/assets/javascripts/application.js
@@ -20,3 +20,19 @@ function sizeOnScroll() {
 }
 
 window.onload = sizeOnScroll();
+
+document.addEventListener('DOMContentLoaded', function(){
+  var elements = document.querySelectorAll('[data-class-toggle]');
+
+  Array.prototype.forEach.call(elements, function(el, i) {
+    var classToToggle = el.getAttribute('data-class-toggle');
+    var elToToggle = document.querySelector(el.getAttribute('data-element'));
+
+    el.addEventListener('click', function(e) {
+      var method = elToToggle.classList.contains(classToToggle) ? 'remove' : 'add';
+      elToToggle.classList[method](classToToggle);
+      e.stopPropagation();
+      e.preventDefault();
+    });
+  });
+});

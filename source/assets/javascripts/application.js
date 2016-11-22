44fca6efb379ffec32ad2c02e1607a371adc1f55
Matteo Scurati <matteo.scurati@gmail.com>
2016-11-22 23:43:00 +0100

    Back

--- source/assets/javascripts/application.js
+++ source/assets/javascripts/application.js
@@ -27,90 +27,18 @@ function sizeOnScroll() {
 
 window.onload = sizeOnScroll();
 
+document.addEventListener('DOMContentLoaded', function(){
+  var elements = document.querySelectorAll('[data-class-toggle]');
 
-function toggleModalOne() {
-  const modalOne = $('#first-dialog');
-  const openOne = $('#first-dialog__open');
-  const closeOne = $('.first-dialog__close');
+  Array.prototype.forEach.call(elements, function(el, i) {
+    var classToToggle = el.getAttribute('data-class-toggle');
+    var elToToggle = document.querySelector(el.getAttribute('data-element'));
 
-  $(openOne).click(() => {
-    if ($(modalOne).hasClass('is-active')) {
-      $(modalOne).removeClass('is-active');
-    } else {
-      $(modalOne).addClass('is-active');
-    }
+    el.addEventListener('click', function(e) {
+      var method = elToToggle.classList.contains(classToToggle) ? 'remove' : 'add';
+      elToToggle.classList[method](classToToggle);
+      e.stopPropagation();
+      e.preventDefault();
+    });
   });
-
-  $(closeOne).click(() => {
-    if ($(modalOne).hasClass('is-active')) {
-      $(modalOne).removeClass('is-active');
-    }
-  });
-};
-
-function toggleModalTwo() {
-  const modalTwo = $('#second-dialog');
-  const openTwo = $('#second-dialog__open');
-  const closeTwo = $('.second-dialog__close');
-
-  $(openTwo).click(() => {
-    if ($(modalTwo).hasClass('is-active')) {
-      $(modalTwo).removeClass('is-active');
-    } else {
-      $(modalTwo).addClass('is-active');
-    }
-  });
-
-  $(closeTwo).click(() => {
-    if ($(modalTwo).hasClass('is-active')) {
-      $(modalTwo).removeClass('is-active');
-    }
-  });
-};
-
-function toggleModalThird() {
-  const modalThird = $('#third-dialog');
-  const openThird = $('#third-dialog__open');
-  const closeThird = $('.third-dialog__close');
-
-  $(openThird).click(() => {
-    if ($(modalThird).hasClass('is-active')) {
-      $(modalThird).removeClass('is-active');
-    } else {
-      $(modalThird).addClass('is-active');
-    }
-  });
-
-  $(closeThird).click(() => {
-    if ($(modalThird).hasClass('is-active')) {
-      $(modalThird).removeClass('is-active');
-    }
-  });
-};
-
-function toggleModalFourth() {
-  const modalFourth = $('#fourth-dialog');
-  const openFourth = $('#fourth-dialog__open');
-  const closeFourth = $('.fourth-dialog__close');
-
-  $(openFourth).click(() => {
-    if ($(modalFourth).hasClass('is-active')) {
-      $(modalFourth).removeClass('is-active');
-    } else {
-      $(modalFourth).addClass('is-active');
-    }
-  });
-
-  $(closeFourth).click(() => {
-    if ($(modalFourth).hasClass('is-active')) {
-      $(modalFourth).removeClass('is-active');
-    }
-  });
-};
-
-$(document).ready(() => {
-  toggleModalOne();
-  toggleModalTwo();
-  toggleModalThird();
-  toggleModalFourth();
 });
